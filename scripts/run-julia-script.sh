7f4685820e8cc750966a02b7490bccb600199a4f
Pete Bachant <pbachant@caltech.edu>
2026-01-18 06:51:29 -0800

    Run amip-baseline

--- scripts/run-julia-script.sh
+++ scripts/run-julia-script.sh
@@ -7,7 +7,20 @@ module load climacommon/2025_05_15
 
 export CLIMACOMMS_DEVICE=CUDA
 
-julia --project=. -e 'using Pkg; Pkg.instantiate(;verbose=true); Pkg.precompile(;strict=true); using CUDA; CUDA.precompile_runtime(); Pkg.status()'
+# Parse --project, which is required
+PROJECT_ARG=""
+for arg in "$@"; do
+    if [[ $arg == --project=* ]]; then
+        PROJECT_ARG=$arg
+        break
+    fi
+done
+if [[ -z $PROJECT_ARG ]]; then
+    echo "Error: --project is required."
+    exit 1
+fi
+
+julia $PROJECT_ARG -e 'using Pkg; Pkg.instantiate(;verbose=true); Pkg.precompile(;strict=true); using CUDA; CUDA.precompile_runtime(); Pkg.status()'
 
 # Pass all args to julia
-julia --project=. "$@"
+julia "$@"
