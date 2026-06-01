07729058ef0821d35dee3bfa15693f7aaf34fd16
Pete Bachant <pbachant@caltech.edu>
2026-06-01 11:57:03 -0700

    Update Julia registry in dev install script

--- scripts/dev-local-repos-coupler.sh
+++ scripts/dev-local-repos-coupler.sh
@@ -5,6 +5,9 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 REPO_ROOT="$(dirname "$SCRIPT_DIR")"
 JULIA_PROJECT="experiments/AMIP"
 
+# First update registry since dev installs don't do this
+julia -e 'using Pkg; Pkg.Registry.update()'
+
 # In ClimaCoupler.jl, dev ClimaCore.jl and ClimaAtmos.jl in the AMIP env
 julia --project="$REPO_ROOT/ClimaCoupler.jl/$JULIA_PROJECT" -e "
     import Pkg
